a44ed8ece4089d7b1b234fb7a4bdf7944d64e3fd
mild59040249101 <mine_5551234@hotmail.com>
2022-12-29 16:09:10 +0700

    จัดสรร ให้ใช้งานง่ายขึ้น

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path')
 const app = express();
-const port = 3000
+const PORT = process.env.PORT;
 
 app.use(morgan('combined'))
 app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/", (req,res) =>{
 
-    res.send('Hello MindKung');
+    res.send('Hello MindKung59040249101 Hight scoll');
 })
 
-app.listen(port, ()=>{
-    debug("Listening on port %d", port)
+app.listen(PORT, ()=>{
+    debug("Listening on port %d", PORT || 4000)
 })
