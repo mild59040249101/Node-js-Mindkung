46fb18279dd12d5082efe35eb53b851e90ad5945
mild59040249101 <mine_5551234@hotmail.com>
2022-12-30 07:14:16 +0700

    app.js

--- app.js
+++ app.js
@@ -14,12 +14,16 @@ app.set("views","./src/views");
 app.set("view engine", "ejs")
 
 productRouter.route("/").get((req,res) =>{
-    res.render("products",
+    res.render("products",{
         products,
-    );
+    }
+    );  
 });
-productRouter.route("/1").get((req,res) =>{
-    res.send("Hello Word !! I'm Product1")
+productRouter.route("/:id").get((req,res) =>{
+    const id = req.params.id;
+    res.render("product",{
+        product: products[id],
+    })
 });
 app.use("/products", productRouter)
 
